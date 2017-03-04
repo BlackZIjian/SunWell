4831a26e4809bc2a61bd558f30a1f717d6ba1200
BlackZIjian <735809461@qq.com>
2017-03-04 12:20:18 +0800

    no message

---
+++ Assets/Test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using PureMVC.Patterns;
+
+public class Test : Proxy {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
